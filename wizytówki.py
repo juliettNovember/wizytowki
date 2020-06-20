aeefb154507a43506a7ae24114a47599d6f7beae
Justyna Nowakowska <nowakowska.just@gmail.com>
2020-06-20 09:59:17 +0200

    Add second class

--- wizytówki.py
+++ wizytówki.py
@@ -2,7 +2,12 @@ from faker import Faker
 fake = Faker()
 
 class baseContact:
-    def __init__ (self, name_and_surname, telephone_number, email:
-        self.name_and_surname = fake.name()
+    def __init__ (self, name_and_surname, telephone_number, email):
+        self.name_and_surname = fake.name(),
         self.telephone_number = telephone_number
         self.email = email
+
+class BusinessContact(baseContact):
+    def __init__(self, position, company, business_number):
+        super().__init__(*args, **kwargs)
+        
