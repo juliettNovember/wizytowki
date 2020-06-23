573550f5b00335d9ddd48f7bdd7649ce82246447
Justyna Nowakowska <nowakowska.just@gmail.com>
2020-06-23 12:52:46 +0200

    Corrects

--- wizytówki.py
+++ wizytówki.py
@@ -8,7 +8,7 @@ class BaseContact:
         self.email = email
 
     def __str__ (self):
-        return f'{name_and_surname} {number} {email}'
+        return f'{self.name_and_surname} {self.number} {self.email}'
 
     def __repr__(self):
         return f"baseContact(name_and_surname={self.name_and_surname}, phone_number={self.phone_number}, email_address={self.email_address})"
@@ -21,49 +21,41 @@ class BaseContact:
         return sum([len(self.first_name), len(self.last_name), 1])
     
 class BusinessContact(BaseContact):
-    def __init__(self, position, company, business_number, *args, **kwargs):
+    def __init__(self, position, company, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.position = position
         self.company = company
-        self.business_number = business_number
-
+        
     def __str__(self):
-        return f'{position} {company} {business_number}'
+        return f'{self.position} {self.company} {self.number}'
 
     def __repr__(self):
-        return f"BusinessContact(company={self.company} position={self.position} business_number={self.business_number})"
+        return f"BusinessContact(company={self.company} position={self.position} business_number={self.number})"
 
     def contact_business(self):
-        return f"Wybieram numer {self.business_number} i dzwonię do {self.name_and_surname}"
+        return (f"Wybieram numer {self.number} i dzwonię do {self.name_and_surname}")
 
     @property
     def label_length(self):
         return sum([len(self.first_name), len(self.last_name), 1])
 
-person = BusinessContact(name_and_surname = fake.name(), number = fake.phone_number(), email = fake.email(), position = fake.job(), company = fake.company(), business_number = fake.phone_number())
-
-card = ("private", "business")
-quantity = 3
-contact = ""
-random_card = []
-
 def create_contacts(card, quantity):
 
     if card == "private":
         for i in range(quantity):
-            contact = f"{fake.name()}, {fake.phone_number()}, {fake.email()}"
-            random_card.append(contact)
+            card = BaseContact(fake.name(), fake.phone_number(), fake.email())
+            print(card)
+
     elif card == "business":
         for i in range (quantity):
-            contact = f"{fake.name()}, {fake.phone_number()}, {fake.email()}, {fake.job()}, {fake.company()}"
-            random_card.append(contact)
+            card = BusinessContact(fake.name(), fake.phone_number(), fake.email(), fake.job(), fake.company())
+            print(card)
+            
     else:
         exit(1)
-    return random_card
-
-
-
-
-print(create_contacts("business", 1))
-print(label_length)
+    return card
+    
+create_contacts("business", 1)
+calling = BusinessContact(fake.job(), fake.company(), fake.name(), fake.phone_number(), fake.email())
+print(calling.contact_business())
 
